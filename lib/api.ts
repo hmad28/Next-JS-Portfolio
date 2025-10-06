41ebf210c0334a7443ebb6cae900b9a263a11cb0
Hammad <email1.hammad@gmail.com>
2025-10-06 15:50:53 +0700

    update portfolio api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,6 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://portfolio-hmd-backend.free.nf/api";
 
 export interface Portfolio {
   id: number;
